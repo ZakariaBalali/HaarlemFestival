5ac6683cbaf5ba20b20582b025aae79fdc57ebdc
ZakariaBalali <50494570+ZakariaBalali@users.noreply.github.com>
2020-11-16 21:34:06 +0100

    Banner, navigationbar en jazz pagina
    Begin gemaakt met de HTML/CSS

---
+++ View/Homepage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Haarlem Festival</title>
+    <meta name="description" content="Haarlem Festival">
+    <meta name="keywords" content="Festival, International, Music, Food, Jazz, Historic">
+    <link href="css/Main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<section class="Banner">
+    <section class="leftBanner">
+        <img class="logoImage" src="images/logo.png">
+        <h1 class="Title">Haarlem Festival</h1>
+    </section>
+    <section class="rightBanner">
+        <h1 class="Date">26-29 July 2021</h1>
+        <form>
+        <fieldset>
+            <p>
+                <label>English</label>
+                <select id = "myList">
+                    <option value = "1">one</option>
+                    <option value = "2">two</option>
+                    <option value = "3">three</option>
+                    <option value = "4">four</option>
+                </select>
+            </p>
+        </fieldset>
+        </form>
+    </section>
+</section>
+<section class="NavigationBar">
+    <a class="active" href="Homepage.php">Home</a>
+    <a href="Dance_Main.php">Dance</a>
+    <a href="Food_Main.php">Food</a>
+    <a href="Jazz_Main.php">Jazz</a>
+    <a href="Historic_Main.php">Historic</a>
+    <a href="Program_Main.php">Program</a>
+</section>
+
+<section class="MainContent">
+
+</section>
+
+<section class="Footer">
+
+</section>
+
+
+</body>
+</html>
---
+++ View/Jazz_Main.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Haarlem Jazz</title>
+    <meta name="description" content="Haarlem Jazz">
+    <meta name="keywords" content="Jazz, smooth, lineup">
+    <link href="css/JazzMain.css" rel="stylesheet" type="text/css">
+    <link href="css/Main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<section class="Banner">
+    <section class="leftBanner">
+        <img class="logoImage" src="images/logo.png">
+        <h1 class="Title">Haarlem Festival</h1>
+    </section>
+    <section class="rightBanner">
+        <h1 class="Date">26-29 July 2021</h1>
+        <form>
+            <fieldset>
+                <p>
+                    <label>English</label>
+                    <select id = "myList">
+                        <option value = "1">one</option>
+                        <option value = "2">two</option>
+                        <option value = "3">three</option>
+                        <option value = "4">four</option>
+                    </select>
+                </p>
+            </fieldset>
+        </form>
+    </section>
+</section>
+<section class="NavigationBar">
+    <a href="Homepage.php">Home</a>
+    <a href="Dance_Main.php">Dance</a>
+    <a href="Food_Main.php">Food</a>
+    <a class="active" href="Jazz_Main.php">Jazz</a>
+    <a href="Historic_Main.php">Historic</a>
+    <a href="Program_Main.php">Program</a>
+</section>
+
+<section class="MainContent">
+    <section class="lineupRoster">
+        <section class="lineupRow">
+            <section class="artist">
+
+            </section>
+        </section>
+    </section>
+
+</section>
+
+<section class="Footer">
+
+</section>
+
+
+</body>
+</html>
