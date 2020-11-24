e696cf00ab5bc8cb37d0f38ba70cf0c8bb518b77
ZakariaBalali <50494570+ZakariaBalali@users.noreply.github.com>
2020-11-24 23:59:31 +0100

    Resize for 125% screen

--- View/Jazz_Ticketpage.php
+++ View/Jazz_Ticketpage.php
@@ -7,6 +7,7 @@
     <meta name="keywords" content="Jazz, smooth, lineup">
     <link href="css/JazzTickets.css" rel="stylesheet" type="text/css">
     <link href="css/Banner.css" rel="stylesheet" type="text/css">
+    <script src="js/JazzScript.js"></script>
 </head>
 <body>
 <section class="Banner">
@@ -48,7 +49,8 @@
         </section>
     </section>
     <section class="tickets">
-        <section class="leftSideTicket1">
+
+        <section class="leftSideTicket1" id="leftSideTicket1">
             <img src="images/GumboKings1.jpg">
             <h1>Gumbo Kings</h1>
             <br>
@@ -57,20 +59,36 @@
             <br>
             <br>
             <p>Morbi semper hendrerit ex eu semper. Sed vitae lectus in nibh facilisis vestibulum at non est.
-                Suspendisse feugiat, odio eget consectetur varius, augue mauris sodales ipsum, sed ultrices mi ipsum eu
+                Suspendisse feugiat, odio eget consectetur varius, augue mauris sodales ipsum, sed ultrices mi ipsum
+                eu
                 sem.</p>
         </section>
-        <section class="rightSideTicket1">
+        <section class="rightSideTicket1" id="rightSideTicket1">
             <label class="dayLabel">Thu 28 Jul</label>
             <label class="timeLabel">18:00-19:00</label>
             <label class="priceLabel">€15,00</label>
             <label class="locationLabel">Patronaat</label>
-            <div class="buyButtonTicketRow1">
-                <img src="images/BuyIcon.png">
-                <label class="buyLabel">Buy Now</label>
+            <div class="buyButtonTicketRow1" id="buyButtonTicketRow1">
+                <a href="javascript:SwapToBuyScreen('leftSideTicket1','rightSideTicket1', 'leftSideTicketBuy1','rightSideTicketBuy1' )">
+                    <img src="images/BuyIcon.png">
+                    <label class="buyLabel">Buy Now</label>
+                </a>
             </div>
         </section>
-        <section class="leftSideTicket2">
+        <section class="leftSideTicketBuy1" id="leftSideTicketBuy1">
+            <div class="closeTicketScreenRow1" id="closeTicketScreenRow1">
+                <a href="javascript:SwapBackToTicketScreen('leftSideTicket1','rightSideTicket1', 'leftSideTicketBuy1','rightSideTicketBuy1' )">
+                    <img src="images/BuyIcon.png">
+                    <label class="closeLabel">Back</label>
+                </a>
+            </div>
+        </section>
+        <section class="rightSideTicketBuy1" id="rightSideTicketBuy1">
+
+        </section>
+
+
+        <section class="leftSideTicket2" id="leftSideTicket2">
             <img src="images/WickedJazzSounds1.jpg">
             <h1>Wicked Jazz Sounds</h1>
             <br>
@@ -81,15 +99,28 @@
             <p>Suspendisse egestas, lorem efficitur lobortis pulvinar, purus mi maximus risus, nec ullamcorper nulla
                 lacus at dolor.</p>
         </section>
-        <section class="rightSideTicket2">
+        <section class="rightSideTicket2" id="rightSideTicket2">
             <label class="dayLabel">Thu 28 Jul</label>
             <label class="timeLabel">18:00-19:00</label>
             <label class="priceLabel">€15,00</label>
             <label class="locationLabel">Patronaat</label>
             <div class="buyButtonTicketRow1">
-                <img src="images/BuyIcon.png">
-                <label class="buyLabel">Buy Now</label>
+                <a href="javascript:SwapToBuyScreen('leftSideTicket2','rightSideTicket2', 'leftSideTicketBuy2','rightSideTicketBuy2' )">
+                    <img src="images/BuyIcon.png">
+                    <label class="buyLabel">Buy Now</label>
+                </a>
             </div>
+        </section>
+        <section class="leftSideTicketBuy2" id="leftSideTicketBuy2">
+            <div class="closeTicketScreenRow1" id="closeTicketScreenRow1">
+                <a href="javascript:SwapBackToTicketScreen('leftSideTicket2','rightSideTicket2', 'leftSideTicketBuy2','rightSideTicketBuy2' )">
+                    <img src="images/BuyIcon.png">
+                    <label class="closeLabel">Back</label>
+                </a>
+            </div>
+        </section>
+        <section class="rightSideTicketBuy2" id="rightSideTicketBuy2">
+
         </section>
         <section class="leftSideTicket3">
             <img src="images/Evolve1.jpg">
@@ -103,6 +134,8 @@
                 tincidunt felis in nunc consectetur viverra. Praesent egestas semper auctor. Cras fringilla pharetra mi,
                 in pulvinar erat venenatis a. Nullam rutrum mollis lacinia.</p>
         </section>
+
+
         <section class="rightSideTicket3">
             <label class="dayLabel">Thu 28 Jul</label>
             <label class="timeLabel">19:30-20:30</label>
---
+++ View/js/JazzScript.js
@@ -0,0 +1,24 @@
+function SwapToBuyScreen(DivTicketLeft, DivTicketRight, DivTicketLeftBuy, DivTicketRightBuy) {
+
+    div1 = document.getElementById(DivTicketLeft);
+    div2 = document.getElementById(DivTicketRight);
+    div3 = document.getElementById(DivTicketLeftBuy);
+    div4 = document.getElementById(DivTicketRightBuy);
+
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "block";
+    div4.style.display = "block";
+}
+
+function SwapBackToTicketScreen(DivTicketLeft, DivTicketRight, DivTicketLeftBuy, DivTicketRightBuy) {
+    div1 = document.getElementById(DivTicketLeft);
+    div2 = document.getElementById(DivTicketRight);
+    div3 = document.getElementById(DivTicketLeftBuy);
+    div4 = document.getElementById(DivTicketRightBuy);
+
+    div1.style.display = "block";
+    div2.style.display = "block";
+    div3.style.display = "none";
+    div4.style.display = "none";
+}
