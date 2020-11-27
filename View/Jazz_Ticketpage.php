94902166de602be992f76ad57fa5988d7a8b0823
ZakariaBalali <50494570+ZakariaBalali@users.noreply.github.com>
2020-11-27 23:42:16 +0100

    Delete unnecessary js file

--- View/Jazz_Ticketpage.php
+++ View/Jazz_Ticketpage.php
@@ -8,7 +8,6 @@
     <link href="css/JazzTickets.css" rel="stylesheet" type="text/css">
     <link href="css/Banner.css" rel="stylesheet" type="text/css">
     <script src="js/JazzScript.js"></script>
-    <script src="js/JazzTicketScript.js" type="text/javascript"></script>
 </head>
 <body>
 
--- View/js/JazzScript.js
+++ View/js/JazzScript.js
@@ -81,3 +81,20 @@ function ShowFriday(ticketThursday, thursdayDayButton, thursdayDayButtonLink, fr
 
 }
 
+function addTicket(minButton, input, addButton) {
+    const minusButton = document.getElementById(minButton);
+    const plusButton = document.getElementById(addButton);
+    const inputField = document.getElementById(input);
+
+    minusButton.addEventListener('click', event => {
+        event.preventDefault();
+        const currentValue = Number(inputField.value) || 0;
+        inputField.value = currentValue - 1;
+    });
+
+    plusButton.addEventListener('click', event => {
+        event.preventDefault();
+        const currentValue = Number(inputField.value) || 0;
+        inputField.value = currentValue + 1;
+    });
+}
