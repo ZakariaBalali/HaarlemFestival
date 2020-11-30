3e142c65e794ce10cf6e07fcdf21a527310d8bb4
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2020-11-30 17:53:42 +0100

    Add files via upload

---
+++ View/js/HistoricScript.js
@@ -0,0 +1,9 @@
+function showPopUp(){
+    document.getElementById("popupBuyTicket").style.visibility="visible";
+}
+function closePopUp(){
+    document.getElementById("popupBuyTicket").style.visibility="hidden";
+}
+/*function blurBackGround(){
+    document.elm.style.filter="blur(0.5rem)"
+}*/
