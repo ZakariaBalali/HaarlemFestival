ee3ed6ffa544bf4051233e77bc869329d6973313
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2020-11-30 17:52:44 +0100

    Historic af

--- View/Historic_Main.php
+++ View/Historic_Main.php
@@ -7,6 +7,7 @@
     <meta name="keywords" content="History, Haarlem, Buildings">
     <link href="css/HistoricMain.css" rel="stylesheet" type="text/css">
     <link href="css/Banner.css" rel="stylesheet" type="text/css">
+    <script src="js/HistoricScript.js"></script>
 </head>
 <body>
 <section class="Banner">
@@ -43,14 +44,11 @@
         </section>
         <section id=covidMeasures>
             <h3>Covid-19 safety measures</h3>
-            <p>1.5 m distance</p>
-            <p>General hand and face hygiene</p>
-            <p>Screening for COVID-19 symptoms before entry</p>
+            <p>1.5 m distance<br>General hand and face hygiene<br>Screening for COVID-19 symptoms before entry</p>
         </section>
         <section id=price>
             <h3>Price</h3>
-            <p>Normal ticket €17,50,- p.p.</p>
-            <p>Family ticket €60,- (4 participants)</p>
+            <p>Normal ticket €17,50,- p.p.<br>Family ticket €60,- (4 participants)</p>
         </section>
         <section id=additionalInfo>
             <h3>Additional information</h3>
@@ -60,12 +58,9 @@
     <section class="programBox">
         <section id=programInfo>
             <h3>Program</h3>
-            <p1>We offer multiple programs. The tours will be in Dutch, English or Chinese. All tours start at the St. Bavo. (Note that some days do not include all different languages). </p1>
-            <p>The price of a normal ticket is €17,50 euros.</p>
-            <p>The price of a family ticket is €60 euros. A family ticket is for 4 persons and is 14% cheaper than a normal ticket. </p>
-            <p>Reservation is mandatory</p>
+            <p>We offer multiple programs. The tours will be in Dutch, English or Chinese. All tours start at the St. Bavo. (Note that some days do not include all different languages).<br><br>The price of a normal ticket is €17,50 euros.<br>The price of a family ticket is €60 euros. A family ticket is for 4 persons and is 14% cheaper than a normal ticket.<br>Reservation is mandatory</p>
         </section>
-        <p id="programQuestion">Select a day and language to see the program</p>
+        <p id="programQuestion"><br>Select a day and language to see the program</p>
         <button class="dayButton">Thu 26 July</button>
         <button class="dayButton">Fri 27 July</button>
         <button class="dayButton">Sat 28 July</button>
@@ -88,49 +83,105 @@
                 <th>Seats left</th>
             </tr>
             <tr>
-                <td>8:00</td>
-                <td>English</td>
-                <td>12</td>
-            </tr>
-            <tr>
                 <td>10:00</td>
                 <td>English</td>
                 <td>12</td>
             </tr>
             <tr>
-                <td>12:00</td>
+                <td>13:00</td>
+                <td>English</td>
+                <td>12</td>
+            </tr>
+            <tr>
+                <td>16:00</td>
                 <td>English</td>
                 <td>12</td>
             </tr>
         </table>
-        <!-- comment-->
-        <form>
-            <button id="buyButton" formaction="#popup">Get your tickets now!</button>
-        </form>
-       <!-- <button id="buyButton" href="#popup">Get your tickets now!</button>-->
+        <button id="buyButton" onclick="showPopUp()">Get your tickets now!</button>
     </section>
-    <section class="popup">
-        <form class=orderTicket>
-        
-        </form>
+    <section id="popupBuyTicket" class="popupBuyTicket">
+        <section id="popupContent">
+            <h1>Order your tickets for the historic event</h1>
+            <form class=orderTicket method="post">
+                <label class="selectDateText" for="selectDate">Select day:</label>
+                <select id="SelectDate" name="selectDate">
+                    <option value="" disabled selected>Select a day</option>
+                    <option value='2021-07-26'>Thur 26 July</option>
+                    <option value='2021-07-27'>Fri 27 July</option>
+                    <option value='2021-07-28'>Sat 28 July</option>
+                    <option value='2021-07-29'>Sun 29 July</option>            
+                </select>
+                <br>
+                <br>
+                <label class="selectTimeText" for="selectTime">Select Time:</label>
+                <select id="SelectTime" name="selectTime">
+                    <option value="" disabled selected>Select a timeslot</option>
+                    <option value='10:00'>10:00</option>
+                    <option value='13:00'>13:00</option>
+                    <option value='16:00'>16:00</option>       
+                </select>
+                <br><br>
+                <label class="selectLanguageText" for="selectLanguage">Select Language:</label>
+                <br>
+                <label class="radioButtonPopUp">
+                    <input type="radio" id="english" name="language" value="english" checked>English
+                </label>
+                <br>
+                <label class="radioButtonPopUp">
+                    <input type="radio" id="dutch" name="language" value="dutch">Dutch
+                </label>
+                <br>
+                <label class="radioButtonPopUp">
+                     <input type="radio" id="chinese" name="language" value="chinese">Chinese
+                </label>
+                <br><br>
+                <label class="selectTicketText" for="selectTicket">Select your ticket:</label>
+                <section id="warningBox">
+                    <section id=warningsMessage>
+                        <h4>Important Note</h4>
+                        <p>participants must be a minimum of 12 years old<br> and no strollers are allowed.</p>
+                    </section>
+                    <section id="tickets">
+                        <section id="normalTicket">
+                            <h3>Normal Ticket</h3>
+                            <p>€17,50</p>
+                            <select id="normalTicketAmount" name="normalTicketAmount">
+                                <option value="" disabled selected>0</option>
+                                <option value='1'>1</option>
+                                <option value='12'>2</option>
+                                <option value='13'>3</option>       
+                            </select>
+                        </section>
+                        <section id="familyTicket">
+                            <h3>Family Ticket</h3>
+                            <p>€60,00</p>
+                            <select id="familyTicketAmount" name="familyTicketAmount">
+                                <option value="" disabled selected>0</option>
+                                <option value='1'>1</option>
+                                <option value='12'>2</option>
+                                <option value='13'>3</option>       
+                            </select>
+                            <h6>(4 participants)</h6>
+                            <img src='images/historic/historic_discount_image.jpg' alt="14% discount" id="discountImage">
+                            <h4>Total</h4>
+                            <p3 id="totalPriceTickets">€0,00</p3>
+                        </section>
+                        <button id="cancelButton" onclick="closePopUp()">Cancel</button>
+                        <button id="addToCartButton" onclick="">Add to cart</button>
+                    </section>
+                </section>
+            </form>
+        </section>
     </section>
     <section class="venuesBox">
         <section id=venuesInfo>
             <h3>Venues</h3>
             <p>In these historic tours we will come across the following 9 venues.</p>
-
             <img src='images/historic/historic_googleMaps_locaties.jpg' alt="Locations of venues which will be visited" id="googleMapsImage">
         </section>
         <section id=venuesTekst>
-            <p>1  Church of St. Bavo</p>
-            <p>2  Grote Markt</p>
-            <p>3  De Hallen</p>
-            <p>4  Proveniershof</p>
-            <p>5  Jopenkerk (Break location)</p>
-            <p>6  Waalse Kerk Haarlem</p>
-            <p>7  Molen de Adriaan</p>
-            <p>8  Amsterdamse Poort</p>
-            <p>9  Hof van Bakenes</p>
+            <p>1  Church of St. Bavo<br>2  Grote Markt<br>3  De Hallen<br>4  Proveniershof<br>5  Jopenkerk (Break location)<br>6  Waalse Kerk Haarlem<br>7  Molen de Adriaan<br>8  Amsterdamse Poort<br>9  Hof van Bakenes </p>
             <img src='images/historic/historic_venues.jpg' id="venuesImage">
         </section>
     </section>
