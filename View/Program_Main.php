b99c3441adad30fc8722d7e58020357a5484b94b
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2020-12-01 10:39:11 +0100

    buttons en filters af

--- View/Program_Main.php
+++ View/Program_Main.php
@@ -29,6 +29,45 @@
 </section>
 
 <section class="MainContent">
+    <section class="buttons">
+        <section class="programButton">
+            <a href="Program_Main.php">PROGRAM</a>
+        </section>
+        <section class="myProgramButton">
+            <a href="Jazz_Ticketpage.php">MY PROGRAM</a>
+        </section>  
+    </section>
+
+    <section class="eventFilter">
+        <h4>Events</h4>
+        <li>
+            <input id="checkBoxDance" type="checkbox" checked value="dance" />
+            <label for="checkBoxLabal">Dance</label>
+        </li>
+        <li>
+            <input id="checkBoxFood" type="checkbox" checked value="food" />
+            <label for="checkBoxLabal">Food</label>
+        </li>
+        <li>
+            <input id="checkBoxJazz" type="checkbox" checked value="jazz" />
+            <label for="checkBoxLabal">Jazz</label>
+        </li>
+        <li>
+            <input id="checkBoxHistoric" type="checkbox" checked value="historic" />
+            <label for="checkBoxLabal">Historic</label>
+        </li>
+    </section>
+    <section class="dayFilter">
+        <h4>Showtimes</h4>
+        <br>
+        <button id="allDaysButton">All days</button>
+        <button id="26thButton">Thu Jul 26th</button>
+        <button id="27thButton">Fri Jul 27th</button>
+        <button id="28thButton">Sat Jul 28th</button>
+        <button id="29thButton">Sun Jul 29th</button>
+
+    </section>  
+
 
 
 </section>
