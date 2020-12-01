0a07fbe21864c753ed83356460edc531498deb0a
salahb10 <47417425+salahb10@users.noreply.github.com>
2020-12-01 09:50:20 +0100

    Add files via upload

---
+++ View/js/DanceScript.js
@@ -0,0 +1,32 @@
+function SwapToBuyScreen(DivTicketTitle, DivTicketDetail, DivTicketLeftBuy, DivTicketRightBuy) {
+
+    div1 = document.getElementById(DivTicketTitle);
+    div2 = document.getElementById(DivTicketDetail);
+    div3 = document.getElementById(DivTicketLeftBuy);
+    div4 = document.getElementById(DivTicketRightBuy);
+
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "block";
+    div4.style.display = "block";
+}
+
+
+
+function addTicket(minButton, input, addButton) {
+    const minusButton = document.getElementById(minButton);
+    const plusButton = document.getElementById(addButton);
+    const inputField = document.getElementById(input);
+
+    minusButton.addEventListener('click', event => {
+        event.preventDefault();
+        const currentValue = Number(inputField.value) || 0;
+        inputField.value = currentValue - 1;
+    });
+
+    plusButton.addEventListener('click', event => {
+        event.preventDefault();
+        const currentValue = Number(inputField.value) || 0;
+        inputField.value = currentValue + 1;
+    });
+}
