5ed9fba1ca1694591d6dc30ad3d9a74fde24ab1e
ZakariaBalali <50494570+ZakariaBalali@users.noreply.github.com>
2021-01-16 01:04:27 +0100

    Create Webhook.php

---
+++ Logic/Webhook.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * How to verify Mollie API Payments in a webhook.
+ *
+ * See: https://docs.mollie.com/guides/webhooks
+ */
+
+try {
+    require_once ("mollie/vendor/autoload.php");
+    require_once ("mollie/examples/functions.php");
+
+    $mollie = new \Mollie\Api\MollieApiClient();
+    $mollie->setApiKey("test_kWeW5tjftHhThty23qAtNeaDyQERjC");
+
+    $payment = $mollie->payments->get($_POST["id"]);
+    $orderId = $payment->metadata->order_id;
+
+    /*
+     * Update the order in the database.
+     */
+    database_write($orderId, $payment->status);
+
+    if ($payment->isPaid() && ! $payment->hasRefunds() && ! $payment->hasChargebacks()) {
+        $_SESSION['StatusPayment'] = "Jevader";
+    } elseif ($payment->isOpen()) {
+        /*
+         * The payment is open.
+         */
+    } elseif ($payment->isPending()) {
+        /*
+         * The payment is pending.
+         */
+    } elseif ($payment->isFailed()) {
+        /*
+         * The payment has failed.
+         */
+    } elseif ($payment->isExpired()) {
+        /*
+         * The payment is expired.
+         */
+    } elseif ($payment->isCanceled()) {
+        /*
+         * The payment has been canceled.
+         */
+    } elseif ($payment->hasRefunds()) {
+        /*
+         * The payment has been (partially) refunded.
+         * The status of the payment is still "paid"
+         */
+    } elseif ($payment->hasChargebacks()) {
+        /*
+         * The payment has been (partially) charged back.
+         * The status of the payment is still "paid"
+         */
+    }
+} catch (\Mollie\Api\Exceptions\ApiException $e) {
+    echo "API call failed: " . htmlspecialchars($e->getMessage());
+}
