7ca4f375cf551d1b62e1ee1f3910654bbb9201cf
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2021-01-18 12:56:25 +0100

    Add files via upload

--- DAL/FoodDAL.php
+++ DAL/FoodDAL.php
@@ -61,6 +61,30 @@ class FoodDAL
             echo 'No Food events found';
         }
     }
+
+    function GetReservation($restaurantName, $startTime)
+    {
+        $sql = "SELECT E.StartTime, E.EndTime , E.Price, F.Event_ID, F.RestaurantName, F.Stars, F.Description, F.Image, F.Cuisine FROM Event E INNER JOIN Event_Food F ON E.Event_ID = F.Event_ID WHERE E.EventName = 'Food' AND E.ProductName = 'Reservation' AND F.RestaurantName = '" . $restaurantName . "' AND E.StartTime = '" . $startTime . "' ORDER BY E.StartTime ";
+        $result = mysqli_query($this->connection, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $StartTime = $row["StartTime"];
+                $EndTime = $row["EndTime"];
+                $Price = $row["Price"];
+                $Event_ID=$row["Event_ID"];
+                $RestaurantName = $row["RestaurantName"];
+                $Stars = $row["Stars"];
+                $Description = $row["Description"];
+                $Image = $row["Image"];
+                $Cuisine = $row["Cuisine"];
+
+                $Food = new Food($Event_ID,$RestaurantName, $Stars, $Description, $Image, $Cuisine, $StartTime, $EndTime, $Price);
+            }
+            return $Food;
+        } else {
+            echo 'No Food events found';
+        }
+    }
 }
 
 ?>
