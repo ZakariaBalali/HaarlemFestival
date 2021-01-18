aae52050acaa54d533aedc9565a565e64945fa15
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2021-01-18 22:10:21 +0100

    Add files via upload

--- View/js/MyProgramScript.js
+++ View/js/MyProgramScript.js
@@ -1,31 +1,29 @@
-function checkBox(id, section) {
-    // Get the checkbox    
+function checkBox(id) {
+    // Get the checkbox
     const checkBox = document.getElementById(id);
 
-    // Get the event sections
-    const events = document.getElementsByName(section);
+    //Sections
+    const sectionThu = document.getElementsByClassName("Thu");
+    const sectionFri = document.getElementsByClassName("Fri");
+    const sectionSat = document.getElementsByClassName("Sat");
+    const sectionSun = document.getElementsByClassName("Sun");
 
-    // const combiEvents = document.getElementsByName(secondSection);
-    //const array = [normalEvents]
-    // if(checkBox.id=="checkBoxDance"){
-    //     const combiEvents = document.getElementsByName("Combi Dance");
-    //     array = [normalEvents, combiEvents]
-    // }
-    // if(checkBox.id=="checkBoxJance"){
-    //     const combiEvents = document.getElementsByName("Combi Jazz");
-    //     array = [normalEvents, combiEvents]
-    // }
+    //array
+    const sectionArray = [sectionThu, sectionFri, sectionSat, sectionSun];
+    console.log(sectionArray);
 
-    //console.log(array);
-
-    events.forEach(function(event) {
-        // checking is checkbox is checked or unchecked
-        if (checkBox.checked){
-            //section will appear
-            event.style.display = "block"; 
-        }else if (!checkBox.cheaked) {
-            //section will disappear
-            event.style.display = "none";
+    sectionArray.forEach(function(event) {
+        for (const e of event) {
+            if(e.className.includes(checkBox.id)){
+                // checking is checkbox is checked or unchecked
+                if (checkBox.checked){
+                    //section will appear
+                    e.style.display = "block"; 
+                }else if (!checkBox.cheaked) {
+                    //section will disappear
+                    e.style.display = "none";
+                }
+            }
         }
     })
 
@@ -33,12 +31,13 @@ function checkBox(id, section) {
   function filterButtons(id){
 
     //Sections
-    const sectionThu = document.getElementById("Thu");
-    const sectionFri = document.getElementById("Fri");
-    const sectionSat = document.getElementById("Sat");
-    const sectionSun = document.getElementById("Sun");
+    const sectionThu = document.getElementsByClassName("Thu");
+    const sectionFri = document.getElementsByClassName("Fri");
+    const sectionSat = document.getElementsByClassName("Sat");
+    const sectionSun = document.getElementsByClassName("Sun");
 
-    let sectionArray = [sectionThu, sectionFri, sectionSat, sectionSun]
+    const sectionArray = [sectionThu, sectionFri, sectionSat, sectionSun];
+
 
     // Get the button
     const button = document.getElementById(id);
@@ -56,26 +55,75 @@ function checkBox(id, section) {
     //change colour of selected button
     button.style.backgroundColor = "#E11B3B";
     button.style.color = "white";
+    console.log(button.id);
 
     if(button.id == "allButton"){
         sectionArray.forEach(function(section) {
-            const sct = document.getElementById(section.id);
-            sct.style.display = "block";
+            for (const e of section) {
+                //show every day section
+                e.style.display = "block"; 
+            }
         })
     }
     else if(button.id == "button26th"){
+        
         sectionArray.forEach(function(section) {
-            //hide every day section
-            const sct = document.getElementById(section.id);
-            //console.log(sct);
-            sct.style.display = "none";
 
-            //show section of selected day
-            if(section.id.includes("Thu")){
-                const sct = document.getElementById(section.id);
-                sct.style.display = "block";
-            }                
-        })
+            for (const e of section) {
+                //hide every day section
+                e.style.display = "none";
+
+                // //show section of selected day
+                if(e.id.includes("Thu")){
+                    e.style.display = "block";
+                }  
+            }
+        })       
+    }
+    else if(button.id == "button27th"){
+        
+        sectionArray.forEach(function(section) {
+
+            for (const e of section) {
+                //hide every day section
+                e.style.display = "none";
+
+                // //show section of selected day
+                if(e.id.includes("Fri")){
+                    e.style.display = "block";
+                }  
+            }
+        })       
+    }
+    else if(button.id == "button28th"){
+        
+        sectionArray.forEach(function(section) {
+
+            for (const e of section) {
+                //hide every day section
+                e.style.display = "none";
+
+                // //show section of selected day
+                if(e.id.includes("Sat")){
+                    e.style.display = "block";
+                }  
+            }
+        })       
+    }
+    else if(button.id == "button29th"){
+        
+        sectionArray.forEach(function(section) {
+
+            for (const e of section) {
+                //hide every day section
+                e.style.display = "none";
+
+                // //show section of selected day
+                if(e.id.includes("Sun")){
+                    e.style.display = "block";
+                }  
+            }
+        })       
     }
   }
     
