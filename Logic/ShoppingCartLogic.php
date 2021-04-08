a7490689998f4bcd59cc33b932bf3b2042012d91
fendyoal <43233547+fendyoal@users.noreply.github.com>
2021-04-08 23:39:40 +0200

    Update ShoppingCartLogic.php

--- Logic/ShoppingCartLogic.php
+++ Logic/ShoppingCartLogic.php
@@ -124,11 +124,7 @@ function AddToSession($EventID, $Amount)
 
     $_SESSION['last_login'] = time();
 
-    // Prevent session hijacking
-    function last_login_is_recent() {
-      $recent_limit = 60 * 60 * 24 * 1; // 1 day
-      if(!isset($_SESSION['last_login'])) { return false; }
-      return (($_SESSION['last_login'] + $recent_limit) >= time());
+   
     }
 }
 
