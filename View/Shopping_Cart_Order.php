6e62ff082d4dc052719fe23adfb322ada14f49cc
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-04-12 12:59:22 +0200

    cancel destroy

--- View/Shopping_Cart_Order.php
+++ View/Shopping_Cart_Order.php
@@ -99,7 +99,7 @@ if($_SESSION['confirm'] != "Confirm"  )
 
 
 <?php
-/// Unset all of the session variables.
+// Unset all of the session variables.
 //$_SESSION = array();
 // Finally, destroy the session.
 //session_destroy();
